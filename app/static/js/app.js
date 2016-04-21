244e74033ea7aaec0aac8562d96c872aae122642
David Martillier <sk3tchk@gmail.com>
2016-04-21 19:09:23 +0000

    initial commit

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,11 @@
 var myApp = angular.module('wishlistApp', []);
 
-    myApp.controller('defaultController', function($scope, $http) {
+    myApp.controller('defaultController', function($scope, $location, $localStorage, $http) {
         $scope.appClickEvent = function(clickEvent) {
             var element = clickEvent.target;
             var img_url = element.src;
             console.log(img_url);
-            
+            // change to $location.path() when you switch to angular routes
             var address_url = location.pathname;
             console.log(address_url);
             
@@ -18,39 +18,44 @@ var myApp = angular.module('wishlistApp', []);
             }).then(function(response){
                 window.location = '/api/user/' + localStorage.getItem('userid');
             });
-        }
+        };
         
        
 });
-    myApp.controller('emailCtrl', function($scope, $http) {
+    myApp.controller('emailController', function($scope, $location, $http) {
         $scope.isPopupVisible = false;
         $scope.isComposePopupVisible = false;
         $scope.composeEmail = {};
         
-        $scope.sendEmail = function() {
-            $scope.isComposePopupVisible = false;
-            alert($scope.composeEmail.to
-                + " " + $scope.composeEmail.subject
-                + " " + $scope.composeEmail.body);
-    };
-        $scope.showComposePopup = function() {
-        $scope.composeEmail = {};
-        $scope.isComposePopupVisible = true;
-    };
-
-         $scope.closeComposePopup = function() {
-        $scope.isComposePopupVisible = false;
-    };
         $scope.showPopup = function(email) {
+        $scope.selectedEmail = email;
         $scope.isPopupVisible = true;
     };
         $scope.closePopup = function() {
-        $scope.isPopupVisible = false;
+            $scope.isPopupVisible = false;
     };
+         $scope.emailSent = false;
+         // change to $location.path() when you switch to angular routes
+         var addressBarUrl = location.pathname;
+            console.log(addressBarUrl);
+            var addressBarUrlParts = addressBarUrl.split("/");
+            console.log(addressBarUrlParts);
+        
+        $scope.sendEmail = function() {
+            $http.post("/api/user/" + addressBarUrlParts[3] + "/wishlists/share", {email: $scope.email})
+            .then(function (response) {
+                $scope.emailSent =true;
+                console.log (response.data);
+                jQuery('#myModal').modal('hide');
+                
+                }, function() {
+                    $scope.emailSent = false;
+                });
+        };
         
         $scope.showComposePopup = function() {
             $scope.composeEmail = {};
             $scope.isComposePopupVisible = true;
-    };
+        };
  
 });
